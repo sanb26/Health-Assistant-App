544f2d9c09a044fcbc897dec602c51388075fb9b
Shantanu Godbole <46984743+shantanugodbole@users.noreply.github.com>
2021-03-24 12:24:20 +0530

    added schedule

--- lib/pages/doctorDetailPage.dart
+++ lib/pages/doctorDetailPage.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart'; //google fonts
 import 'package:health_assistant/DatabaseManager/DatabaseManager.dart';
 import 'package:health_assistant/pages/appointment.dart';
 import 'package:health_assistant/theme/light_color.dart';
+import 'package:http/http.dart';
 
 // ignore: camel_case_types
 class doctorDetail extends StatefulWidget {
@@ -23,16 +24,8 @@ class _doctorDetailState extends State<doctorDetail> {
 
   _doctorDetailState(this.doctorId, this.pID);
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    // fetchDocDetails();
-  }
-
   fetchDocDetails() async {
     var resultant = await DatabaseManager().getDoctorDetails(doctorId);
-
     if (resultant == null) {
       print('Unable to retrieve the doctors list');
     } else {
@@ -41,6 +34,16 @@ class _doctorDetailState extends State<doctorDetail> {
       // });
       return resultant;
     }
+  }
+
+  Future<List<QueryDocumentSnapshot>> getScheduleInfo(docId) async {
+    var data = await FirebaseFirestore.instance
+        .collection('doctors')
+        .doc(docId)
+        .collection('Schedule')
+        .get();
+
+    return data.docs;
   }
 
   @override
@@ -56,98 +59,151 @@ class _doctorDetailState extends State<doctorDetail> {
           );
         }
         var docDetails = snapshot.data;
-        return Scaffold(
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: LightColor.purple,
-            title: Text(docDetails['type'][0].toUpperCase() +
-                docDetails['type'].substring(1)),
-          ),
-          body: SafeArea(
-              child: Stack(
-            children: [
-              Image.network(docDetails['profile_image'],
-                  width: MediaQuery.of(context).size.width,
-                  fit: BoxFit.contain),
-              DraggableScrollableSheet(
-                  maxChildSize: .8,
-                  initialChildSize: .5,
-                  minChildSize: .5,
-                  builder: (context, scrollController) {
-                    return Container(
-                      height: MediaQuery.of(context).size.height * .5,
-                      padding: EdgeInsets.only(left: 19, right: 19, top: 16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30),
-                            topRight: Radius.circular(30)),
-                        color: Colors.white,
-                      ),
-                      child: SingleChildScrollView(
-                        physics: BouncingScrollPhysics(),
-                        controller: scrollController,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(docDetails['name'],
-                                style: GoogleFonts.lato(
-                                    fontSize:
-                                        MediaQuery.of(context).size.height / 30,
-                                    fontWeight: FontWeight.bold)),
-                            Text(docDetails['degree'],
-                                style: GoogleFonts.lato(
-                                    fontSize:
-                                        MediaQuery.of(context).size.height / 42,
-                                    color: LightColor.subTitleTextColor)),
-                            Text(
-                                docDetails['experience'].toString() +
-                                    " years of experience\n",
-                                style: GoogleFonts.lato(
-                                    fontSize:
-                                        MediaQuery.of(context).size.height / 42,
-                                    color: LightColor.subTitleTextColor)),
-                            Text("About",
-                                style: GoogleFonts.lato(
-                                    fontSize:
-                                        MediaQuery.of(context).size.height / 30,
-                                    fontWeight: FontWeight.bold)),
-                            Text(docDetails['description'],
-                                style: GoogleFonts.lato(
-                                    fontSize:
-                                        MediaQuery.of(context).size.height / 42,
-                                    color: LightColor.lightblack)),
-                            SizedBox(height: 20),
-                            Center(
-                              child: RaisedButton(
-                                  color: LightColor.purple,
-                                  child: Text("Book Appointment",
-                                      style: GoogleFonts.lato(
-                                          fontSize: MediaQuery.of(context)
-                                                  .size
-                                                  .height /
-                                              35,
-                                          color: Colors.white)),
-                                  onPressed: () {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                AppointmentPage(
-                                                    doctorId, pID)));
-                                    // Navigator.push(
-                                    //     context,
-                                    //     MaterialPageRoute(
-                                    //         builder: (context) =>
-                                    //             DoctorSchedule(doctorId)));
-                                  }),
+        return FutureBuilder<List<QueryDocumentSnapshot>>(
+          future: getScheduleInfo(doctorId),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Scaffold(body: Center(child: CircularProgressIndicator()));
+            }
+            var scheduleDetails = snapshot.data;
+            return Scaffold(
+              appBar: AppBar(
+                automaticallyImplyLeading: false,
+                backgroundColor: LightColor.purple,
+                title: Text(docDetails['type'][0].toUpperCase() +
+                    docDetails['type'].substring(1)),
+              ),
+              body: SafeArea(
+                  child: Stack(
+                children: [
+                  Image.network(docDetails['profile_image'],
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.contain),
+                  DraggableScrollableSheet(
+                      maxChildSize: .8,
+                      initialChildSize: .5,
+                      minChildSize: .5,
+                      builder: (context, scrollController) {
+                        return Container(
+                          height: MediaQuery.of(context).size.height * .5,
+                          padding:
+                              EdgeInsets.only(left: 19, right: 19, top: 16),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30),
+                                topRight: Radius.circular(30)),
+                            color: Colors.white,
+                          ),
+                          child: SingleChildScrollView(
+                            physics: BouncingScrollPhysics(),
+                            controller: scrollController,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(docDetails['name'],
+                                    style: GoogleFonts.lato(
+                                        fontSize:
+                                            MediaQuery.of(context).size.height /
+                                                30,
+                                        fontWeight: FontWeight.bold)),
+                                Text(docDetails['degree'],
+                                    style: GoogleFonts.lato(
+                                        fontSize:
+                                            MediaQuery.of(context).size.height /
+                                                42,
+                                        color: LightColor.subTitleTextColor)),
+                                Text(
+                                    docDetails['experience'].toString() +
+                                        " years of experience\n",
+                                    style: GoogleFonts.lato(
+                                        fontSize:
+                                            MediaQuery.of(context).size.height /
+                                                42,
+                                        color: LightColor.subTitleTextColor)),
+                                Text("About",
+                                    style: GoogleFonts.lato(
+                                        fontSize:
+                                            MediaQuery.of(context).size.height /
+                                                30,
+                                        fontWeight: FontWeight.bold)),
+                                Text(docDetails['description'],
+                                    style: GoogleFonts.lato(
+                                        fontSize:
+                                            MediaQuery.of(context).size.height /
+                                                42,
+                                        color: LightColor.lightblack)),
+                                SizedBox(height: 20),
+                                Text("Schedule",
+                                    style: GoogleFonts.lato(
+                                        fontSize:
+                                            MediaQuery.of(context).size.height /
+                                                30,
+                                        fontWeight: FontWeight.bold)),
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: scheduleDetails.length,
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      child: ListTile(
+                                        title: Text(
+                                          scheduleDetails[index].data()['day'],
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 24),
+                                        ),
+                                        subtitle: Text(
+                                            scheduleDetails[index]
+                                                    .data()['in_time'] +
+                                                " to " +
+                                                scheduleDetails[index]
+                                                    .data()['out_time'],
+                                            style: GoogleFonts.lato(
+                                                fontSize: MediaQuery.of(context)
+                                                        .size
+                                                        .height /
+                                                    42,
+                                                color: LightColor
+                                                    .subTitleTextColor)),
+                                      ),
+                                    );
+                                  },
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Center(
+                                  child: RaisedButton(
+                                      color: LightColor.purple,
+                                      child: Text("Book Appointment",
+                                          style: GoogleFonts.lato(
+                                              fontSize: MediaQuery.of(context)
+                                                      .size
+                                                      .height /
+                                                  35,
+                                              color: Colors.white)),
+                                      onPressed: () {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    AppointmentPage(
+                                                        doctorId, pID)));
+                                        // Navigator.push(
+                                        //     context,
+                                        //     MaterialPageRoute(
+                                        //         builder: (context) =>
+                                        //             DoctorSchedule(doctorId)));
+                                      }),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }),
-            ],
-          )),
+                          ),
+                        );
+                      }),
+                ],
+              )),
+            );
+          },
         );
       },
     );
