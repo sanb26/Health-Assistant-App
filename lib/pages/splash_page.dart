72fb1dfc29da3855893a2311795d6173302abd21
Ojas Kulkarni <ojas.k@somaiya.edu>
2021-04-25 18:14:30 +0530

    User stays logged in after app is killed

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:health_assistant/pages/sign_in.dart';
 // import 'package:health_assistant/pages/homePagePatient.dart';
@@ -6,6 +8,11 @@ import 'package:health_assistant/theme/light_color.dart';
 import 'package:health_assistant/theme/text_styles.dart';
 import 'package:health_assistant/theme/extention.dart';
 import 'package:google_fonts/google_fonts.dart'; //google fonts
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:health_assistant/pages/homepagePatient.dart';
+import 'package:health_assistant/pages/sign_in.dart';
+import 'package:health_assistant/DatabaseManager/DatabaseManager.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SplashPage extends StatefulWidget {
   SplashPage({Key key}) : super(key: key);
@@ -14,15 +21,63 @@ class SplashPage extends StatefulWidget {
   _SplashPageState createState() => _SplashPageState();
 }
 
+/*Future.delayed(Duration(seconds: 2)).then((_) {
+Navigator.pushReplacement(
+context, MaterialPageRoute(builder: (_) => SignIn()));
+}); */
+
 class _SplashPageState extends State<SplashPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 2)).then((_) {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (_) => SignIn()));
-    });
     super.initState();
+    startTimer();
   }
+  static Future<bool> checkIfDocExists(String docId) async {
+    try {
+      // Get reference to Firestore collection
+      var collectionRef = FirebaseFirestore.instance.collection('patients');
+      var doc = await collectionRef.doc(docId).get();
+      return doc.exists;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  // Function to return User Information
+  Future<DocumentSnapshot> getUserInfo(String uid) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    var qs = await firestore.collection('patients').doc(uid).get();
+    if (qs.exists) {
+      return qs;
+    } else {
+      return null;
+    }
+  }
+  void startTimer() {
+    Timer(Duration(seconds: 1), () {
+      navigateUser(); //It will redirect  after 3 seconds
+    });
+  }
+  //final FirebaseAuth _auth = FirebaseAuth.instance;
+  void navigateUser() async{
+    if (FirebaseAuth.instance.currentUser != null) {
+      User user = FirebaseAuth.instance.currentUser;
+      print("Logged in!!");
+      bool val = await checkIfDocExists(user.uid);
+      DocumentSnapshot userData = await getUserInfo(user.uid);
+      if (val && userData.exists) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => PatientHomeScreen(
+                uid: user.uid,
+                lname: userData.data()['lname'],
+                fname: userData.data()['fname'])));
+      }
+    } else {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignIn()));
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
